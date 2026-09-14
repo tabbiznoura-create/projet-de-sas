2d4b069850758e1bbe5791243d15cb9cf345866f
tabbiznoura-create <tabbiznoura@gmail.com>
2026-09-14 17:48:08 +0200

    day04

---
+++ day03/exercices/exercice-12.js
@@ -0,0 +1,39 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 03 · EXERCICE 12 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * LE CALLBACK BASIQUE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Créez une fonction executerOperation(a, b, operation)
+ * . operation sera une fonction passée en argument.
+ *  Appelez : executerOperation(5, 3, multiplier) 
+ * (en utilisant la fonction multiplier créée 
+ * plus tôt).
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-12
+ * ▶️ Commande : node day03/exercices/exercice-12.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+
+function multiplier(a, b){
+    return a * b
+}
+function addition(a, b){
+    return a + b
+}
+function sustraction(a,b){
+    return a - b
+}
+function division(a,b){
+    return a / b
+}
+function executerOperation(a, b, opearation){
+    return opearation(a , b) 
+}
+
+console.log(executerOperation(5, 3,sustraction))
+
