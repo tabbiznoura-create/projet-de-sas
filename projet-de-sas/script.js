04bcfbf46ecc5e99af61ee30179b8be4e5b24244
tabbiznoura-create <tabbiznoura@gmail.com>
2026-09-16 17:29:34 +0200

    add project files

---
+++ projet-de-sas/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+const apprenants = [
+  {
+    id: 1,
+    nomComplet: "Sara Dev",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 18,
+        totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 14,
+        totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 2,
+    nomComplet: "Yassine Code",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 12,
+        totalExercices: 20, challengeTermine: false }
+    ]
+  }
+]
